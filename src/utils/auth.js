ba614e557350dafffeab3f917c13a354fb4b56d7
Shantanu <shantanuwable2003@gmail.com>
2024-04-02 12:44:25 +0530

    feat: add supabase auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,112 +1,154 @@
-import { useEffect, useState, createContext, Children, useContext } from "react";
-import { auth } from './firebase'
-import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import {
+    useEffect,
+    useState,
+    createContext,
+    Children,
+    useContext,
+} from "react";
+import { auth } from "./firebase";
+import {
+    GithubAuthProvider,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+} from "firebase/auth";
 import { useRouter } from "next/router";
-import { toast } from "react-hot-toast"
+import { toast } from "react-hot-toast";
+import { createClient } from "@supabase/supabase-js";
 
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
-
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-
     const auth = useAuth();
-    return (
-        <AuthContext.Provider value={auth}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+    return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
 
 export const UserAuth = () => {
     return useContext(AuthContext);
-}
+};
 const useAuth = () => {
     const [user, setUser] = useState(null);
     const router = useRouter();
 
     useEffect(() => {
-        const localUser = JSON.parse(localStorage.getItem('user'));
+        const localUser = JSON.parse(localStorage.getItem("user"));
         if (localUser) {
             setUser(localUser);
         }
         if (user) {
-            router.push('/dashboard');
+            router.push("/dashboard");
         }
-        console.log(user)
-    }, [])
-
-
+        console.log(user);
+    }, []);
 
     const googleSignIn = async () => {
         const googleProvider = new GoogleAuthProvider();
-        return signInWithPopup(auth, googleProvider).then((res) => {
-            console.log("User Signed In!!!")
-            console.log(res.user)
+        return signInWithPopup(auth, googleProvider)
+            .then((res) => {
+                console.log("User Signed In!!!");
+                console.log(res.user);
 
-            localStorage.setItem('user', JSON.stringify(res.user));
-            toast.success("Login Successful")
-            router.push('/dashboard')
-        }).catch((error) => {
-            console.log(error.message)
-        })
-    }
-
-    const signInGithub = () => {
-        const githubProvider = new GithubAuthProvider()
-        return signInWithPopup(auth, githubProvider).then((res) => {
-            console.log("User Signed In!!!")
-            console.log(res.user)
-            localStorage.setItem('user', JSON.stringify(res.user));
-            toast.success("Login Succes")
-            router.push('/dashboard')
-
-        }).catch((error) => {
-            console.log(error.message)
-        })
-    }
-
-    const emailSignIn = async (email, password) => {
-        try {
-            // Try to sign in the user
-            const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
-            console.log("User Signed In!!!");
-            console.log(user);
-
-            localStorage.setItem('user', JSON.stringify(user));
-            router.push('/dashboard')
-        } catch (signInError) {
-            // If user is not found, sign up the user
-            if (signInError.code === 'auth/user-not-found') {
-                try {
-                    const newUserCredential = await createUserWithEmailAndPassword(auth, email, password);
-                    const newUser = newUserCredential.user;
-                    console.log("User Signed Up!!!");
-                    toast.success("Login Succes")
-                    console.log(newUser);
-                    router.push('/dashboard')
-
-                } catch (signUpError) {
-                    console.error(signUpError.message);
-                }
-            } else {
-                console.error(signInError.message);
-            }
-        }
+                localStorage.setItem("user", JSON.stringify(res.user));
+                toast.success("Login Successful");
+                router.push("/dashboard");
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
     };
 
+    const signInGithub = () => {
+        const githubProvider = new GithubAuthProvider();
+        return signInWithPopup(auth, githubProvider)
+            .then((res) => {
+                console.log("User Signed In!!!");
+                console.log(res.user);
+                localStorage.setItem("user", JSON.stringify(res.user));
+                toast.success("Login Succes");
+                router.push("/dashboard");
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    };
+
+    const emailSignIn = async (email, password) => {
+        // try {
+        // // Try to sign in the user
+        // const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        // const user = userCredential.user;
+        // console.log("User Signed In!!!");
+        // console.log(user);
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (error) {
+            console.error(error.message);
+            if (error.code == 404) {
+                const { data, error } = await supabase.auth.signUp({
+                    email,
+                    password,
+                });
+                console.log("Data: ", data, "\nError: ", error);
+
+                if (error) {
+                    console.error(error);
+                } else {
+                    localStorage.setItem("user", JSON.stringify(data.user));
+                    localStorage.setItem(
+                        "session",
+                        JSON.stringify(data.session)
+                    );
+                    router.push("/dashboard");
+                }
+            }
+        } else {
+            localStorage.setItem("user", JSON.stringify(data.user));
+            localStorage.setItem("session", JSON.stringify(data.session));
+            router.push("/dashboard");
+        }
+        // } catch (signInError) {
+        //     // If user is not found, sign up the user
+        //     if (signInError.code === "auth/user-not-found") {
+        //         try {
+        //             const newUserCredential =
+        //                 await createUserWithEmailAndPassword(
+        //                     auth,
+        //                     email,
+        //                     password
+        //                 );
+        //             const newUser = newUserCredential.user;
+        //             console.log("User Signed Up!!!");
+        //             toast.success("Login Succes");
+        //             console.log(newUser);
+        //             router.push("/dashboard");
+        //         } catch (signUpError) {
+        //             console.error(signUpError.message);
+        //         }
+        //     } else {
+        //         console.error(signInError.message);
+        //     }
+        // }
+    };
 
     const signOut = () => {
         return auth.signOut().then(() => {
-            console.log("User Signed Out!!!")
-            toast.success("Logout Successful")
-            localStorage.removeItem('user');
+            console.log("User Signed Out!!!");
+            toast.success("Logout Successful");
+            supabase.auth.signOut();
+            localStorage.removeItem("user");
             setUser(null);
-            router.push('/')
-        })
-    }
+            router.push("/");
+        });
+    };
 
-    return { user, setUser, googleSignIn, signInGithub, emailSignIn, signOut }
-
-}
+    return { user, setUser, googleSignIn, signInGithub, emailSignIn, signOut };
+};
