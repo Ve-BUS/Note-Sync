9f3ddce14834c91c699d05a81db15da6553b883b
Shantanu <shantanuwable2003@gmail.com>
2024-04-02 14:42:01 +0530

    feat: utils file for shadcn

---
+++ src/utils/utils.js
@@ -0,0 +1,17 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs) {
+    return twMerge(clsx(inputs));
+}
+
+export function extractFileName(fullName) {
+    const parts = fullName.split(".");
+    parts.pop();
+    return parts.join(".");
+}
+
+export function extractFileNameFromPath(path) {
+    const parts = path.split("/");
+    return parts[parts.length - 1];
+}
