3c6a4491740e842676367cdefbd8037b67ea9d7a
Shantanu <shantanuwable2003@gmail.com>
2024-04-02 14:40:46 +0530

    feat: add nextUI provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import "@/src/styles/globals.css";
 import { AuthProvider } from "@/src/utils/auth";
 import { ThemeProvider } from "next-themes";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }) {
     return (
         <ThemeProvider attribute="class">
-            <AuthProvider>
-                <Component {...pageProps} />
-            </AuthProvider>
+            <NextUIProvider>
+                <AuthProvider>
+                    <Component {...pageProps} />
+                </AuthProvider>
+            </NextUIProvider>
         </ThemeProvider>
     );
 }
