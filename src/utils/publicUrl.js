43dd8aed57f01772a03628354f9e2ee5213906c2
Mustafa <100032108+TechyMT@users.noreply.github.com>
2024-04-05 23:20:40 +0530

    Update API URL in publicUrl.js

--- src/utils/publicUrl.js
+++ src/utils/publicUrl.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-    // return "https://hack-o-rama.onrender.com/api";
-    return "http://localhost:8000/api";
+    return "https://hack-o-rama.onrender.com/api";
+    // return "http://localhost:8000/api";
 
 };
